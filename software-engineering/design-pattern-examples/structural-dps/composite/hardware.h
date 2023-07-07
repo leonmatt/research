47bb2da3a7015fdf5744f76737c9e43c82e8ca9f
Matthew Leon <99342476+leonmatt@users.noreply.github.com>
2023-07-06 20:38:30 -0700

    Development (#28)
    * Fix merge conflicts
    
    This commit fixes a bunch of compounded merge conflicts
    
    Signed-off-by: Matthew Leon <matthew.leon@intel.com>
    
    * Recode everything and split server/client
    
    This commit re-organizes the code, splits the main program
    into a server and client since we don't want to have to multi-thread,
    and adds a setup script for placing each nic in its own network namespace.
    
    Signed-off-by: Matthew Leon <matthew.leon@intel.com>
    
    * Add working server and client code
    
    This commit fixes some issues and cleans up the server
    and client. This is a buggy yet working solution.
    
    Signed-off-by: Matthew Leon <matthew.leon@intel.com>
    
    * Add routes to setup script and clean up code
    
    This commit adds important routes to the setup steps
    so that the RDMA driver knows the proper routes for
    communicating
    
    Signed-off-by: Matthew Leon <matthew.leon@intel.com>
    
    * Add Factory design pattern example
    
    This commit adds the factory design pattern example to my research
    repository
    
    Signed-off-by: Matthew Leon <matthew.leon@intel.com>
    
    * Add documentation for factory method
    
    This commit adds more documentation and a README for the factory method pattern
    
    Signed-off-by: Matthew Leon <matthew.leon@intel.com>
    
    * Clean RDMAConnection
    
    This commit updates header guards and syntax for the RDMA connection class
    
    Signed-off-by: Matthew Leon <matthew.leon@intel.com>
    
    * Add abstract factory outline
    
    Signed-off-by: Matthew Leon <matthew.leon@intel.com>
    
    * Add appliance and furniture classes
    
    Signed-off-by: Matthew Leon <matthew.leon@intel.com>
    
    * Add README for abstract factory method
    
    Signed-off-by: Matthew Leon <matthew.leon@intel.com>
    
    * Add builder skeleton code and refactor repo
    
    This commit gives an outline for the builder design pattern
    along with refactoring the repo's design patterns into their
    descriptive directories
    
    Signed-off-by: Matthew Leon <matthew.leon@intel.com>
    
    * Finish Builder and Add Main
    
    This commit finishes the Builder example design pattern
    along with adding a main driver
    
    Signed-off-by: Matthew Leon <matthew.leon@intel.com>
    
    * Add README for Builder
    
    This commit adds a README for the Builder pattern code example
    
    Signed-off-by: Matthew Leon <matthew.leon@intel.com>
    
    * Add Prototype Pattern
    
    This commit adds the Prototype Pattern example. It also fixes
    issues in the other design pattern examples
    
    Signed-off-by: Matthew Leon <matthew.leon@intel.com>
    
    * Add Singleton example
    
    This commit adds the Singleton design pattern
    
    Signed-off-by: Matthew Leon <matthew.leon@intel.com>
    
    * Add README for Singleton
    
    This commit adds the README for the singleton pattern
    
    Signed-off-by: Matthew Leon <matthew.leon@intel.com>
    
    * Add outline for adapter and clean up
    
    This commit adds the outline for the adapter design pattern
    along with cleaning existing patterns
    
    Signed-off-by: Matthew Leon <matthew.leon@intel.com>
    
    * Add Adapter implementation
    
    This commit adds the implementation of the Adapter
    pattern
    
    Signed-off-by: Matthew Leon <matthew.leon@intel.com>
    
    * Update READMEs
    
    This commit updates READMEs for prototype, singleton, and adapter
    patterns
    
    Signed-off-by: Matthew Leon <matthew.leon@intel.com>
    
    * Add Bridge pattern
    
    This commit adds the Bridge design pattern code example
    
    Signed-off-by: Matthew Leon <matthew.leon@intel.com>
    
    * Add Bridge README
    
    Signed-off-by: Matthew Leon <matthew.leon@intel.com>
    
    * Add Composite Pattern Code
    
    This commit implements the Composite Design Pattern
    
    Signed-off-by: Matthew Leon <matthew.leon@intel.com>
    
    ---------
    
    Signed-off-by: Matthew Leon <matthew.leon@intel.com>

---
+++ software-engineering/design-pattern-examples/structural-dps/composite/hardware.h
@@ -0,0 +1,156 @@
+/*
+Copyright (C) 2023 Matthew Leon
+SPDX-License-Identifier: BSD-3-Clause
+*/
+
+#ifndef HARDWARE_H
+#define HARDWARE_H
+
+#include <string>
+#include <iostream>
+#include <vector>
+#include <memory>
+
+using namespace std;
+
+class HardwareDevice
+{
+
+public:
+
+    virtual void build() {}
+
+};
+
+class XEONCPU : HardwareDevice
+{
+
+public:
+
+    string name;
+
+    XEONCPU() {}
+    ~XEONCPU() {}
+
+    void build() override
+    {
+
+        cout << "We built a XEON CPU" << endl;
+
+    }
+
+};
+
+class CoreCPU : HardwareDevice
+{
+
+public:
+
+    string name;
+
+    CoreCPU() {}
+    ~CoreCPU() {}
+
+    void build() override
+    {
+
+        cout << "We built a Core CPU" << endl;
+
+    }
+
+};
+
+class E810NIC : HardwareDevice
+{
+
+public:
+
+    string name;
+
+    E810NIC() {}
+    ~E810NIC() {}
+
+    void build() override
+    {
+
+        cout << "We built an E810 NIC" << endl;
+
+    }
+
+};
+
+class IPUNIC : HardwareDevice
+{
+
+public:
+
+    string name;
+
+    IPUNIC() {}
+    ~IPUNIC() {}
+
+    void build() override
+    {
+
+        cout << "We built an IPU NIC" << endl;
+
+    }
+
+};
+
+class HardwareGroup
+{
+
+public:
+
+    vector<shared_ptr<HardwareDevice>> deviceVector;
+
+    HardwareGroup() {}
+    ~HardwareGroup() {}
+
+    void buildAllDevices()
+    {
+
+        for (shared_ptr<HardwareDevice> i : deviceVector)
+            i->build();
+
+        deviceVector.clear();
+
+    }
+
+    void addDevice(shared_ptr<HardwareDevice> device)
+    {
+        deviceVector.push_back(device);
+    }
+
+    void removeDevice()
+    {
+        deviceVector.pop_back();
+    }
+
+};
+
+shared_ptr<HardwareDevice> getHardwareDevice(string kind)
+{
+
+    HardwareDevice *ptr = NULL;
+
+    if (kind.compare("XEON") == 0)
+        ptr = (HardwareDevice *)new XEONCPU();
+
+    else if (kind.compare("Core") == 0)
+        ptr = (HardwareDevice *)new CoreCPU();
+
+    if (kind.compare("E810") == 0)
+        ptr = (HardwareDevice *)new E810NIC();
+
+    else if (kind.compare("IPU") == 0)
+        ptr = (HardwareDevice *)new IPUNIC();
+
+    shared_ptr<HardwareDevice> ret(ptr);
+
+    return ret;
+
+}
+
+#endif // HARDWARE_H
