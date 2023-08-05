d1f4a4225e635c4fa5a5c186d2a9b25604828367
Matthew Leon <matthew.leon@intel.com>
2023-08-05 20:19:32 +0000

    Add Logistic Regression Example
    This commit adds a Logistic Regression code example
    
    Signed-off-by: Matthew Leon <matthew.leon@intel.com>

--- artificial-intelligence/machine-learning/supervised-learning/regression/logistic-regression.py
+++ artificial-intelligence/machine-learning/supervised-learning/regression/logistic-regression.py
@@ -6,29 +6,29 @@ SPDX-License-Identifier: BSD-3-Clause
 # Import required modules
 import numpy as np
 
-from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import LogisticRegression
+from sklearn.datasets import make_classification
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import mean_squared_error
+from sklearn.metrics import accuracy_score
 
 # Set nummpy random seed
 seed = 42
 np.random.seed(seed)
 
 # Generate the data
-X = np.random.rand(10000, 1)
-Y = 72 + X * np.random.rand(10000, 1)
+X, Y = make_classification(n_samples=10000, n_features=10, n_informative=7, random_state=seed)
 
 # Split the data so we can fit and test
 X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.2, random_state=seed)
 
 # Initialize and fit the model to our generated data
-LRModel = LinearRegression()
+LRModel = LogisticRegression()
 LRModel.fit(X_train, y_train)
 
 # Test the model
 y_prediction = LRModel.predict(X_test)
 
 # Get the accuracy
-mse = mean_squared_error(y_test, y_prediction)
+accuracy_score = accuracy_score(y_test, y_prediction)
 
-print(f"The Model's Mean Squared Error is: {mse}")
+print(f"The Model's Accuracy is: {accuracy_score * 100:.2f}%")
