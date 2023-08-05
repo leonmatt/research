1fa878f88837840dff98671031ff5357daa883cf
Matthew Leon <99342476+leonmatt@users.noreply.github.com>
2023-08-05 15:23:02 -0700

    Cloud examples (#62)
    * Add basic main program for CAP Theorem example
    
    This commit adds a basic program that will be used to demonstrate
    the CAP theorem
    
    Signed-off-by: Matthew Leon <matthew.leon@intel.com>
    
    * Add CA part of CAP Theorem example
    
    This commit adds the code for Consistency and Availability demonstration
    to the CAP Theorem code example
    
    Signed-off-by: Matthew Leon <matthew.leon@intel.com>
    
    * Add Network Partitioning tradeoff to CAP demo
    
    This commit implements the Partitioning tradeoff to the
    CAP Theorem code example
    
    Signed-off-by: Matthew Leon <matthew.leon@intel.com>
    
    ---------
    
    Signed-off-by: Matthew Leon <matthew.leon@intel.com>

---
+++ cloud/distributed-systems/cap-theorem/Orchestrator.java
@@ -0,0 +1,96 @@
+/*
+Copyright (C) 2023 Matthew Leon
+SPDX-License-Identifier: BSD-3-Clause
+*/
+
+import java.util.*;
+
+public class Orchestrator
+{
+
+    public int numberOfNodes;
+    public int capQualities;
+    private Vector<Node> nodes;
+
+    public Orchestrator(int numNodes, int qualitiesChoice)
+    {
+
+        // Set up the orchestator with the nodes
+        setupOrchestrator(numNodes, qualitiesChoice);
+
+        System.out.println("Finished setting up the Orchestrator\n");
+
+    }
+
+    public void startOrchestrator()
+    {
+
+        switch(capQualities) {
+            case 1:
+                System.out.println("Running with Consistency and Availability\n");
+                System.out.println("The system is unable to handle network errors\n");
+                break;
+            case 2:
+                System.out.println("Running with Consistency and Partitioning\n");
+                break;
+            case 3:
+                System.out.println("Running with Availability and Partitioning\n");
+                break;
+        }
+
+        System.out.println("Starting the Orchestrator\n");
+
+        Random rng = new Random();
+        int newValue;
+        boolean continueProcessing = true;
+
+        while (continueProcessing) {
+
+            // Consistency and Availability
+            if (capQualities == 1) {
+
+                newValue = rng.nextInt(100);
+                System.out.println("Writing " + newValue + " to the Nodes\n");
+                for (int i = 0; i < numberOfNodes; i++)
+                    nodes.get(i).write(newValue);
+
+                newValue = rng.nextInt(numberOfNodes);
+
+                System.out.println("Value stored in Node " + newValue + " is: " + nodes.get(newValue).read() + "\n");
+
+                try {
+
+                    Thread.sleep(500);
+
+                } catch (InterruptedException e) {}
+
+                if (rng.nextInt(10) <= 1) {
+
+                    System.out.println("The Network Partition just failed and we are unable to recover\n");
+
+                    continueProcessing = false;
+
+                }
+
+            }
+
+        }
+
+    }
+
+    private void setupOrchestrator(int numNodes, int qualities)
+    {
+
+        nodes = new Vector<Node>();
+
+        for(int i = 0; i < numNodes; i++)
+            nodes.add(new Node());
+
+        System.out.println("We added " + numNodes + " nodes to the system\n\n");
+
+        numberOfNodes = numNodes;
+        capQualities = qualities;
+
+    }
+
+}
