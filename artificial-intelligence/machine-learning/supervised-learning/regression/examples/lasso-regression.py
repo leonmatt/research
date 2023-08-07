a4ec7bf36c974198ad9bbab937cb7e5ab29082bc
Matthew Leon <99342476+leonmatt@users.noreply.github.com>
2023-08-06 21:39:00 -0700

    Artificial intelligence examples (#65)
    * Add Logistic Regression Example
    
    This commit adds a Logistic Regression code example
    
    Signed-off-by: Matthew Leon <matthew.leon@intel.com>
    
    * Add Multiple Linear Regression
    
    This commit adds the Multiple Linear Regression and cleans up the repo
    
    Signed-off-by: Matthew Leon <matthew.leon@intel.com>
    
    * Add polynomial, ridge, and lasso regression
    
    This commit adds the polynomial, ridge, and lasso regression examples
    
    Signed-off-by: Matthew Leon <matthew.leon@intel.com>
    
    ---------
    
    Signed-off-by: Matthew Leon <matthew.leon@intel.com>

---
+++ artificial-intelligence/machine-learning/supervised-learning/regression/examples/lasso-regression.py
@@ -0,0 +1,38 @@
+'''
+Copyright (C) 2023 Matthew Leon
+SPDX-License-Identifier: BSD-3-Clause
+'''
+
+# Import required modules
+import numpy as np
+
+from sklearn.datasets import make_regression
+from sklearn.linear_model import Lasso
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error
+
+# Set nummpy random seed
+seed = 42
+np.random.seed(seed)
+
+# Generate the data
+X, Y = make_regression(n_samples=10000, n_features=3, noise=0.15, random_state=seed)
+polynomial_feat = PolynomialFeatures(degree=2, include_bias=False)
+x_poly = polynomial_feat.fit_transform(X)
+Y = Y + np.sum(x_poly[:, 1:], axis=1)
+
+# Split the data so we can fit and test
+X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.2, random_state=seed)
+
+# Initialize and fit the model to our generated data
+LRModel = Lasso(alpha=0.1)
+LRModel.fit(X_train, y_train)
+
+# Test the model
+y_prediction = LRModel.predict(X_test)
+
+# Get the accuracy
+mse = mean_squared_error(y_test, y_prediction)
+
+print(f"The Model's Mean Squared Error is: {mse}")
