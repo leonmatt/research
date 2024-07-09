783e7e064e66bdbe4bd497f73ed0788aa063c0b1
Matthew Leon <sp4rt4h@gmail.com>
2024-07-09 16:49:39 -0700

    Add hello_world program using hash maps
    Signed-off-by: Matthew Leon <sp4rt4h@gmail.com>

---
+++ linux/kernel/epbf_learning/hello_world_maps.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+
+from bcc import BPF
+
+from time import sleep
+
+program = r"""
+BPF_HASH(counter_table);
+
+int hello_world(void *ctx) {
+
+    u64 uid = bpf_get_current_uid_gid() & 0xFFFFFFFF;
+    u64 counter = 0;
+    u64 *p = counter_table.lookup(&uid);
+    if (p) {
+    
+        counter = *p;
+    
+    }
+
+    counter++;
+
+    counter_table.update(&uid, &counter);
+
+    return 0;
+
+}
+"""
+
+b = BPF(text=program)
+syscall = b.get_syscall_fnname("execve")
+b.attach_kprobe(event=syscall, fn_name="hello_world");
+
+while True:
+        sleep(2)
+        s = ""
+        for k,v in b["counter_table"].items():
+            s += f"ID {k.value}: {v.value}\n"
+        print(s)
